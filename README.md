1b05f5b76bb00bfd9787f18f4a95e571241083c8
boedegoat <bhremada.fka@gmail.com>
2024-12-12 11:38:53 +0700

    init project

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 ## Starting Local Server
 
-1. `dfx start`
+1. `dfx start --clean`
 2. `dfx generate backend`
-3. `npm i && npm run build`
-4. setup `.env`
+3. setup `.env`, add `NEXT_PUBLIC_CANISTER_ID_BACKEND` set it to match `CANISTER_ID_BACKEND`
+4. `npm i && npm run build`
 5. `dfx deploy`
+
+## Starting Frontend Dev Server
+
+Follows the same steps as above, then run:
+
+```bash
+npm run dev
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useQueryCall } from '@/lib/actor'
 export default function Home() {
 	const { data: greet, call: hi } = useQueryCall({
 		functionName: 'hi',
-		args: ['asep'],
+		args: ['guest'],
 	})
 
 	const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
@@ -24,11 +24,16 @@ export default function Home() {
 	}
 
 	return (
-		<main>
-			<h1>ICP Next.js</h1>
-			<form onSubmit={onSubmit}>
-				<input className='bg-transparent' type='text' name='name' placeholder='name' />
-				<button>greet</button>
+		<main className='p-10'>
+			<h1 className='font-bold text-4xl'>ICP Ticket</h1>
+			<form onSubmit={onSubmit} className='my-5 flex gap-2'>
+				<input
+					className='bg-transparent border border-gray-500 rounded-md p-0.5'
+					type='text'
+					name='name'
+					placeholder='name'
+				/>
+				<button className='bg-blue-500 px-3 font-semibold py-0.5 rounded-md'>greet</button>
 			</form>
 			<div>result: {greet}</div>
 		</main>
